acd2b88ff0674835b600ed4e6238a804e8450b4b
Harrison Winkler <harryewinkler@thinkful.com>
2020-11-12 10:52:29 -0500

    finishing correct endpoint

---
+++ spaced-repetition/build/precache-manifest.70457966e7119d7b00b8cdfaf7dad3ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c2a9004f656ae3ef219",
+    "url": "/static/css/main.a66ec72d.chunk.css"
+  },
+  {
+    "revision": "7c2a9004f656ae3ef219",
+    "url": "/static/js/main.7c2a9004.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "52e7b454b902a08a97eb",
+    "url": "/static/js/2.52e7b454.chunk.js"
+  },
+  {
+    "revision": "850db1af8b415836c1dc85bbbf0ec8b6",
+    "url": "/index.html"
+  }
+];
